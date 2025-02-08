df7e323de29083d5ce5a2d8f576a3d0b682ae738
Yulia Antiuk <uliaantiuk@gmail.com>
2025-02-08 18:45:11 +0200

    1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Express працює!");
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
