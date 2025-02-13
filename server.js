bfa212a5af92a9b9aeb65fc481c08429abab57d7
Quaswexnt <dgarkovenko10@gmail.com>
2025-02-13 17:26:46 +0200

    check branch

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello KPI!");
 });
+app.get("/test", (req, res) => {
+  res.send("Hello 52!");
+})
 
 app.listen(3000, () => {
   console.log("Сервер запущено на http://localhost:3000");
