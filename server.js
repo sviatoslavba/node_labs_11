708361b23e4384a7aec033f489e0badd84518983
Quaswexnt <dgarkovenko10@gmail.com>
2025-02-14 18:28:05 +0200

    first version of dynamic page

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello KPI!");
+// Устанавливаем EJS как шаблонизатор
+app.set('view engine', 'ejs');
+
+// Данные студента (можно заменить на JSON или БД)
+const student = {
+  name: 'Іван Петренко',
+  age: 20,
+  university: 'Київський національний університет',
+  course: 2
+};
+
+// Роут для отображения страницы студента
+app.get('/student', (req, res) => {
+  res.render('student', { student });
 });
-app.get("/test", (req, res) => {
-  res.send("Hello 52!");
-})
-app.get("/dynamic", (req, res) => {
-  res.send("Hello 11!");
+
+app.get('/', (req, res) => {
+  res.get("Sometgin");
 })
 
-app.listen(3000, () => {
-  console.log("Сервер запущено на http://localhost:3000");
-});
+// Запуск сервера
+app.listen(3000, () => console.log('Сервер запущен на http://localhost:3000'));
