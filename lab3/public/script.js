970665b96c0b597386c81b4e9df162776550eeb2
Quaswexnt <dgarkovenko10@gmail.com>
2025-03-29 15:32:30 +0200

    Implemented several petition functions

--- lab3/public/script.js
+++ lab3/public/script.js
@@ -1,12 +1,80 @@
-document.querySelector('.create-btn').addEventListener('click', () => {
-    window.location.href = '/api/petition-creation';
-});
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const petitionCreationLeave = document.querySelector('.btn.cansel-btn');
+    if(petitionCreationLeave) {
+        petitionCreationLeave.addEventListener('click', () => {
+            window.location.href = '/api/petitions';
+        });
+    }
 
 
-document.querySelectorAll('.details-btn').forEach(button => {
-    button.addEventListener('click', (event) => {
-        const petitionId = event.target.dataset.id;
-        window.location.href = `/api/petition-overview/${petitionId}`;
 
+
+    const petitionCreationForm = document.querySelector('.create-btn');
+    if(petitionCreationForm) {
+        petitionCreationForm.addEventListener('click', () => {
+            window.location.href = '/api/petition-creation';
+        })
+    }
+
+    const petitionDetails = document.querySelectorAll('.details-btn');
+
+    petitionDetails.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const petitionId = event.target.dataset.id;
+            window.location.href = `/api/petition-overview/${petitionId}`;
+        });
     });
-});
+
+
+    const searchInput = document.getElementById('searchInput');
+    if (searchInput) {
+        const petitionCards = document.querySelectorAll('.petition-card');
+        searchInput.addEventListener('input', (event) => {
+            const searchIndex = searchInput.value.toLowerCase();
+            petitionCards.forEach(card => {
+                const title = card.querySelector("h3").textContent.toLowerCase();
+                card.style.display = title.includes(searchIndex) ? "block" : "none";
+            });
+        });
+    }
+    const createBtn = document.querySelector(".btn.action-btn");
+    if (createBtn) {
+        createBtn.addEventListener("click", async(event) => {
+            console.log("Кнопка натиснута");
+            event.preventDefault();
+
+            const title = document.getElementById("pet-title")?.value.trim();
+            const text = document.getElementById("pet-text")?.value.trim();
+
+            if (!title || !text) {
+                alert("Заповніть всі поля!!");
+                return;
+            }
+
+            const response = await fetch("/api/petition-creation", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ title, text })
+            });
+
+            if (!response.ok) {
+                alert("Сталася помилка! Перевірте консоль.");
+                console.error(await response.text());
+                return;
+            }
+
+            const result = await response.json();
+            alert(`Петиція створена, її номер: ${result.data.petition.id}`);
+        });
+    }
+})
+
+
+
+
+
+
+
+
--- lab3/server.js
+++ lab3/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -6,12 +8,15 @@ const morgan = require('morgan');
 const app = express()
 const PORT = 3000;
 
+
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 const petitions = JSON.parse(fs.readFileSync(path.join(__dirname, 'petitions.json'), 'utf8'));
 
@@ -19,10 +24,6 @@ app.get(["/","/api/petitions"], (req, res) => {
     res.render('index', {petitions});
 
 })
-
-
-
-
 
 app.get("/api/petition-creation", (req, res) => {
     res.render('create-petition');
@@ -45,5 +46,56 @@ app.get("/api/petition-overview/:id", (req, res) => {
     });
 })
 
+app.post("/api/petition-creation",(req, res) => {
+    const {title, text} = req.body;
+
+
+    if(!title || !text) {
+        return  res.status(400).json({
+            status: 'fail',
+            message:"Заповніть всі поля"
+        })
+    }
+
+    const author_id = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    const newId = petitions.length+1;
+    const creation_date = new Date();
+    const expiry_date = new Date();
+
+    expiry_date.setMonth(creation_date.getMonth() + 1);
+
+    const formatted_creation_date = creation_date.toISOString().split('T')[0];
+    const formatted_expiry_date = expiry_date.toISOString().split('T')[0];
+
+    const petition = {
+        id:newId,
+        author_id,
+        title,
+        text,
+        petition_current:0,
+        creation_date:formatted_creation_date,
+        expiry_date:formatted_expiry_date
+
+    }
+    petitions.push(petition);
+
+    fs.writeFile("petitions.json", JSON.stringify(petitions,null,2),(err)=>{
+        if(err){
+            console.error(err.message);
+            return res.status(500).json({
+                status:'fail',
+                message:err.message,
+            });
+        }
+        console.log("Дані були записані")
+        res.status(200).json({
+            status:'success',
+            data:{
+                petition
+            }
+        });
+
+    });
+});
 
 app.listen(PORT,()=>console.log(`Server started on ${PORT}`));
